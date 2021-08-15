297cca384f3a8e0adeca15e145ca5fb738ea2646
Lucjan Suski <lucjansuski@gmail.com>
2021-08-15 13:07:36 +0200

    init

---
+++ abc.js
@@ -0,0 +1,2 @@
+import React from "./_snowpack/pkg/react.js";
+export const Abc = () => /* @__PURE__ */ React.createElement("div", null, "asddefzxcasd");
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-/* Add JavaScript code here! */
-console.log('Hello World! You did it! Welcome to Snowpack :D');
+import React from "./_snowpack/pkg/react.js";
+import ReactDOM from "./_snowpack/pkg/react-dom.js";
+import {Abc} from "./abc.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Abc, null), '"asd HELLO REACT"'), document.getElementById("root"));
